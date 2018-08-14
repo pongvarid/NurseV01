2032aabae9260d7371ec7618d4879fdf0ee01b10
Pongv <pongvarid@gmail.com>
2018-08-15 00:58:33 +0700

    Auto stash before merge of "master" and "origin/master"

--- resources/views/teacher/profile.blade.php
+++ resources/views/teacher/profile.blade.php
@@ -5,34 +5,55 @@ $user = isset($_SESSION['user']);
 if(!$user){ echo '<meta http-equiv="refresh" content="0; url=/" />';}else{
     $id = $_SESSION['user'];
 }
-/*-------------------SET SESSION-----------------------*/
-include(resource_path().'/alert/alert.php');
-?>
-
+ 
+?> 
 @extends('core.vuetify') 
- 
- <style>
-     .bgh{ 
-background: #111 url(https://www.img.in.th/images/4d8c442b2cdb2583584d7e63d6c87017.jpg)  no-repeat left top;
-background-size: cover; 
- 
-    }
-    
- </style>   
 @section('vue')
- 
-<?php
- include(resource_path().'/views/teacher/test.blade.php');
-alert();
-?>
+<v-container >
+    <v-layout row>
+    <v-flex xs12 sm4  >
+        <v-card>
+            <v-toolbar color="light-blue" dark>
+                <v-toolbar-side-icon><v-icon>fas fa-user-circle</v-icon></v-toolbar-side-icon>
+                <v-toolbar-title>เกี่ยวกับคุณ</v-toolbar-title>
+                <v-spacer></v-spacer>
+            </v-toolbar>
+            <v-list two-line subheader>
+             <h2>dsds</h2>
+            </v-list>
+        </v-card>
+    </v-flex>
+    <v-flex xs12 sm1></v-flex>
+    <v-flex xs12 sm8 >
+        <v-card>
+            <v-toolbar color="light-blue" dark>
+                <v-toolbar-side-icon></v-toolbar-side-icon>
+                <v-toolbar-title>รายวิชาที่เปิดสอน</v-toolbar-title>
+                <v-spacer></v-spacer>
+                <v-btn dark icon>
+                        <v-icon>fas fa-plus-circle </v-icon>
+                       
+                      </v-btn>
+                      
+            </v-toolbar>
+            <v-list two-line subheader>
+                <v-subheader inset>Folders</v-subheader> 
+                <v-divider inset></v-divider>
+                <v-subheader inset>Files</v-subheader>
+                 
+            </v-list>
+        </v-card>
+    </v-flex>
+</v-layout>
+</v-container>
 @endsection
  
 @section('vue_script')
 <script>
-  new Vue({
+    new Vue({
   el: "#app",
   data: {
-      <?=script_alert()?>
+ 
    teacher:{}
   },
   methods: { 
@@ -53,7 +74,6 @@ alert();
       this.load();
   }
 });
-
 
 </script>
 @endsection

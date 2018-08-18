bf513ab854905b435ac7cea350548b7abdce7b26
June <bennita42@gmail.com>
2018-08-18 15:05:20 +0700

    Models update

--- app/Models/Exercise.php
+++ app/Models/Exercise.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Exercise extends Model
 {
-    //
+    protected $table = "exercise";
+    protected $fillable = [
+        "course",
+        "type",
+        "name",
+        "score",
+        "ask",
+        "answer",
+        "remark",
+        "time"
+
+    ];
 }

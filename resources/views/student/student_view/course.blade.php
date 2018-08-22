4facf2b3ff723d07687d5ee0e5deafc4a325a63c
meile8 <bennita42@gmail.com>
2018-08-23 00:32:04 +0700

    student edit

--- resources/views/student/student_view/course.blade.php
+++ resources/views/student/student_view/course.blade.php
@@ -10,7 +10,7 @@ else{
         echo '<meta http-equiv="refresh" content="0; url=/teacher/profile" />'; die();
     } 
 }
-?>
+?> 
 @extends('core.vuetify') 
 @section('vue')
 <v-container grid-list-md>
@@ -19,25 +19,25 @@ else{
             <v-card color="">
                 <v-toolbar color="indigo" dark>
                     <v-icon>fas fa-user-circle </v-icon>
-                    <v-toolbar-title>ข้อมูลรายวิชา @{{courses.name}} [@{{courses.code}}]</v-toolbar-title>
+                    <v-toolbar-title>ข้อมูลรายวิชา @{{courses.name.split(',')[1]}} (@{{courses.name.split(',')[2]}}) [@{{courses.code}}]</v-toolbar-title>
                     <v-spacer></v-spacer>
                 </v-toolbar>
                 <v-card-text>
                     <v-layout row wrap>
                         <v-flex xs12 sm6 md3>
-                            <v-text-field v-model="courses.name"  label="ชื่อรายวิชา" outline append-icon="fas fa-book" readonly></v-text-field>             
+                            <v-text-field v-model="courses.name.split(',')[1]" label="ชื่อรายวิชา" outline append-icon="fas fa-book" readonly></v-text-field>
                         </v-flex>
                         <v-flex xs12 sm6 md3>
-                            <v-text-field v-model="courses.code"  label="รหัสรายวิชา" outline append-icon="fas fa-dice" readonly></v-text-field>             
+                            <v-text-field v-model="courses.code" label="รหัสรายวิชา" outline append-icon="fas fa-dice" readonly></v-text-field>
                         </v-flex>
                         <v-flex xs12 sm6 md3>
-                            <v-text-field v-model="courses.year"  label="ปีการศึกษา" outline append-icon="far fa-calendar-alt" readonly></v-text-field>             
+                            <v-text-field v-model="courses.year" label="ปีการศึกษา" outline append-icon="far fa-calendar-alt" readonly></v-text-field>
                         </v-flex>
                         <v-flex xs12 sm6 md3>
-                            <v-text-field v-model="courses.teacher"  label="อาจารย์" outline append-icon="fas fa-user-tie" readonly></v-text-field>             
+                            <v-text-field v-model="teacher.name" label="อาจารย์" outline append-icon="fas fa-user-tie" readonly></v-text-field>
                         </v-flex>
                         <v-flex xs12 sm6 md3>
-                            <v-text-field v-model="courses.created_at"  label="วันที่เปิดรายวิชา" outline append-icon="far fa-clock" readonly></v-text-field>             
+                            <v-text-field v-model="courses.created_at" label="วันที่เปิดรายวิชา" outline append-icon="far fa-clock" readonly></v-text-field>
                         </v-flex>
                     </v-layout>
                 </v-card-text>
@@ -49,13 +49,13 @@ else{
         <v-flex d-flex xs12 sm4>
             <v-card>
                 <v-toolbar color="indigo" dark>
-                <v-icon>fas fa-server</v-icon>
+                    <v-icon>fas fa-server</v-icon>
                     <v-toolbar-title>ข้อมูลเอกสาร</v-toolbar-title>
                     <v-spacer></v-spacer>
                 </v-toolbar>
                 <v-card-text>
                     <div v-for="document in documents">
-                        <v-btn  large color="primary" block @click="goto_filePage(document.link)">@{{document.name}}</v-btn>
+                        <v-btn large color="primary" block @click="goto_filePage(document.link)">@{{document.name}}</v-btn>
                     </div>
                 </v-card-text>
             </v-card>
@@ -63,21 +63,21 @@ else{
         <v-flex d-flex xs12 sm4>
             <v-card>
                 <v-toolbar color="indigo" dark>
-                <v-icon>far fa-clipboard</v-icon>
+                    <v-icon>far fa-clipboard</v-icon>
                     <v-toolbar-title>ข้อมูลแบบฝึกหัด</v-toolbar-title>
                     <v-spacer></v-spacer>
                 </v-toolbar>
                 <v-card-text>
                     <v-card-text>
-                            <div v-for="exercise in exercises">
-                                    <v-list-tile avatar @click="goto_exercisePage(exercise.id,exercise.type)">
-                                        <v-list-tile-content>
-                                            <p class="headline mb-0">@{{exercise.name}}</p>
-                                            <div>@{{exercise.time}}</div> 
-                                        </v-list-tile-content>
-                                    </v-list-tile>
-                                <v-divider></v-divider>
-                            </div>
+                        <div v-for="exercise in exercises">
+                            <v-list-tile avatar @click="goto_exercisePage(exercise.id,exercise.type)">
+                                <v-list-tile-content>
+                                    <p class="headline mb-0">@{{exercise.name}}</p>
+                                    <div>@{{exercise.time}}</div>
+                                </v-list-tile-content>
+                            </v-list-tile>
+                            <v-divider></v-divider>
+                        </div>
                     </v-card-text>
                 </v-card-text>
             </v-card>
@@ -85,7 +85,7 @@ else{
         <v-flex d-flex xs12 sm4>
             <v-card>
                 <v-toolbar color="indigo" dark>
-                <v-icon>fas fa-book-reader</v-icon>
+                    <v-icon>fas fa-book-reader</v-icon>
                     <v-toolbar-title>นิสิตที่ลงเรียน</v-toolbar-title>
                     <v-spacer></v-spacer>
                 </v-toolbar>
@@ -104,6 +104,7 @@ else{
   el: "#app",
   data: {  
    courses:{},
+   teacher:{},
    exercises:{},
    dataCheck:1,
    register:{
@@ -145,9 +146,6 @@ else{
           alert('error: '+e);
         });
       },
-      load(){
-        this.getCourse();
-      },
       getExercise(){
         let result = axios.get("/api/exercise_data/{{request()->route('id')}}")
         .then((r)=>{
@@ -156,10 +154,19 @@ else{
           alert('error: '+e);
         });
       },
+      getTeacher(){
+        let result = axios.get("/api/teacher/{{request()->route('id')}}")
+        .then((r)=>{
+          this.teacher = r.data;
+        }).catch((e)=>{
+          alert('error: '+e);
+        });
+      },
       load(){
         this.getCourse();
         this.getExercise();
         this.getDocument();
+        this.getTeacher();
       },
   },
   mounted(){
@@ -169,4 +176,3 @@ else{
 
 </script>
 @endsection
-

b202c046195f8d8c42ae53cd78ffde2dcb0ade69
meile8 <bennita42@gmail.com>
2018-08-24 02:55:48 +0700

    edit register course

--- resources/views/course/course_view/register_course.blade.php
+++ resources/views/course/course_view/register_course.blade.php
@@ -37,7 +37,7 @@ else{
                             <v-text-field v-model="teacher.name" label="อาจารย์" outline append-icon="place" readonly></v-text-field>
                         </v-flex>
                         <v-flex xs12 sm6 md3>
-                            <v-text-field v-model="courses.created_at" label="วันที่เปิดรายวิชา" outline append-icon="place" readonly></v-text-field>
+                            <v-text-field v-model="time" label="วันที่เปิดรายวิชา" outline append-icon="place" readonly></v-text-field>
                         </v-flex>
                     </v-layout>
                 </v-card-text>
@@ -52,11 +52,11 @@ else{
                     <v-toolbar-title>ข้อมูลเอกสาร</v-toolbar-title>
                     <v-spacer></v-spacer>
                 </v-toolbar>
-                    <v-container>
-                        <div v-for="document in documents">
-                            <v-btn  color="primary" block @click="goto_filePage(document.link)">@{{document.name}}</v-btn>
-                        </div>
-                    </v-container>  
+                <v-container>
+                    <div v-for="document in documents">
+                        <v-btn color="primary" block @click="goto_filePage(document.link)">@{{document.name}}</v-btn>
+                    </div>
+                </v-container>
             </v-card>
         </v-flex>
         <v-flex d-flex xs12 sm4>
@@ -71,7 +71,7 @@ else{
                             <v-list-tile>
                                 <v-list-tile-content>
                                     <p class="headline mb-0">@{{exercise.name}}</p>
-                                    <div>@{{exercise.time}}</div>
+                                    <div>@{{time2}}</div>
                                 </v-list-tile-content>
                             </v-list-tile>
                             <v-divider></v-divider>
@@ -86,59 +86,62 @@ else{
                     <v-toolbar-title>นิสิตที่ลงเรียน</v-toolbar-title>
                     <v-spacer></v-spacer>
                 </v-toolbar>
-                    <v-container>
-                        <v-btn block style="background-color:#683ECF;" dark > <v-icon>far fa-clipboard</v-icon>&nbspคะแนน</v-btn>
-                        <v-btn block style="background-color:#683ECF;" dark  @click="studentDialog = true"><v-icon>fas fa-user-graduate</v-icon>&nbspข้อมูลนิสิต</v-btn>
-                        <v-btn block style="background-color:#683ECF;" dark  @click="studentDialogTA = true"><v-icon>fas fa-user-shield</v-icon>&nbspข้อมูล TA</v-btn>
-                    </v-container>
+                <v-container>
+                    <v-btn block style="background-color:#683ECF;" dark>
+                        <v-icon>far fa-clipboard</v-icon>&nbspคะแนน</v-btn>
+                    <v-btn block style="background-color:#683ECF;" dark @click="studentDialog = true">
+                        <v-icon>fas fa-user-graduate</v-icon>&nbspข้อมูลนิสิต</v-btn>
+                    <v-btn block style="background-color:#683ECF;" dark @click="studentDialogTA = true">
+                        <v-icon>fas fa-user-shield</v-icon>&nbspข้อมูล TA</v-btn>
+                </v-container>
             </v-card>
         </v-flex>
     </v-layout>
 </v-container>
 <v-dialog v-model="studentDialog" fullscreen hide-overlay transition="dialog-bottom-transition">
-        <v-card>
-            <v-toolbar dark color="primary">
-                <v-btn icon dark @click.native="studentDialog = false">
-                    <v-icon>close</v-icon>
-                </v-btn>
-                <v-toolbar-title>ข้อมูลนิสิต</v-toolbar-title>
-                <v-spacer></v-spacer>
-            </v-toolbar>
-            <br><br>
-            <div v-for="students in student">
-                <div v-if="students.permission == 1">
-                    <v-container>
-                        <h5>(@{{JSON.parse(students.studentIn.data).StudentCode}}) @{{JSON.parse(students.studentIn.data).FirstName_TH}}&nbsp;@{{JSON.parse(students.studentIn.data).LastName_TH}}</h5>
-                        <p><b>@{{JSON.parse(students.studentIn.data).FacultyName_TH}}</b>@{{JSON.parse(students.studentIn.data).CourseName_TH}}
-                        </p>
-                        <v-divider></v-divider>
-                    </v-container>
-                </div>
+    <v-card>
+        <v-toolbar dark color="primary">
+            <v-btn icon dark @click.native="studentDialog = false">
+                <v-icon>close</v-icon>
+            </v-btn>
+            <v-toolbar-title>ข้อมูลนิสิต</v-toolbar-title>
+            <v-spacer></v-spacer>
+        </v-toolbar>
+        <br><br>
+        <div v-for="students in student">
+            <div v-if="students.permission == 1">
+                <v-container>
+                    <h5>(@{{JSON.parse(students.studentIn.data).StudentCode}}) @{{JSON.parse(students.studentIn.data).FirstName_TH}}&nbsp;@{{JSON.parse(students.studentIn.data).LastName_TH}}</h5>
+                    <p><b>@{{JSON.parse(students.studentIn.data).FacultyName_TH}}</b>@{{JSON.parse(students.studentIn.data).CourseName_TH}}
+                    </p>
+                    <v-divider></v-divider>
+                </v-container>
             </div>
-        </v-card>
-    </v-dialog>
-    <v-dialog v-model="studentDialogTA" fullscreen hide-overlay transition="dialog-bottom-transition">
-            <v-card>
-                <v-toolbar dark color="primary">
-                    <v-btn icon dark @click.native="studentDialogTA = false">
-                        <v-icon>close</v-icon>
-                    </v-btn>
-                    <v-toolbar-title>ข้อมูล TA</v-toolbar-title>
-                    <v-spacer></v-spacer>
-                </v-toolbar>
-                <br><br>
-                <div v-for="students in student">
-                    <div v-if="students.permission == 2">
-                        <v-container>
-                            <h5>(@{{JSON.parse(students.studentIn.data).StudentCode}}) @{{JSON.parse(students.studentIn.data).FirstName_TH}}&nbsp;@{{JSON.parse(students.studentIn.data).LastName_TH}}</h5>
-                            <p><b>@{{JSON.parse(students.studentIn.data).FacultyName_TH}}</b>@{{JSON.parse(students.studentIn.data).CourseName_TH}}
-                            </p>
-                            <v-divider></v-divider>
-                        </v-container>
-                    </div>
-                </div>
-            </v-card>
-        </v-dialog>
+        </div>
+    </v-card>
+</v-dialog>
+<v-dialog v-model="studentDialogTA" fullscreen hide-overlay transition="dialog-bottom-transition">
+    <v-card>
+        <v-toolbar dark color="primary">
+            <v-btn icon dark @click.native="studentDialogTA = false">
+                <v-icon>close</v-icon>
+            </v-btn>
+            <v-toolbar-title>ข้อมูล TA</v-toolbar-title>
+            <v-spacer></v-spacer>
+        </v-toolbar>
+        <br><br>
+        <div v-for="students in student">
+            <div v-if="students.permission == 2">
+                <v-container>
+                    <h5>(@{{JSON.parse(students.studentIn.data).StudentCode}}) @{{JSON.parse(students.studentIn.data).FirstName_TH}}&nbsp;@{{JSON.parse(students.studentIn.data).LastName_TH}}</h5>
+                    <p><b>@{{JSON.parse(students.studentIn.data).FacultyName_TH}}</b>@{{JSON.parse(students.studentIn.data).CourseName_TH}}
+                    </p>
+                    <v-divider></v-divider>
+                </v-container>
+            </div>
+        </div>
+    </v-card>
+</v-dialog>
 @endsection
  
 @section('vue_script')
@@ -176,7 +179,6 @@ else{
         axios.get("/api/check?student={{$_SESSION['student']}}&course={{request()->route('id')}}")
         .then((r)=>{
             this.dataCheck = r.data;
-          
         }).catch((e)=>{
 
         });
@@ -204,6 +206,14 @@ else{
         let result = axios.get("/api/course_data/{{request()->route('id')}}")
         .then((r)=>{
           this.courses = r.data;
+          this.conTime = this.courses.updated_at.split(' ')[0];
+          this.time = moment(this.conTime).format('L');
+          let result = axios.get("/api/teacher/"+this.courses.teacher)
+        .then((r)=>{
+          this.teacher = r.data;
+        }).catch((e)=>{
+          alert('error: '+e);
+        });
         }).catch((e)=>{
           alert('error: '+e);
         });
@@ -211,21 +221,13 @@ else{
       getExercise(){
         let result = axios.get("/api/exercise_data/{{request()->route('id')}}")
         .then((r)=>{
-          this.exercises = r.data;
-        }).catch((e)=>{
-          alert('error: '+e);
-        });
-      },
-      getTeacher(){
-        let result = axios.get("/api/teacher/{{request()->route('id')}}")
-        .then((r)=>{
-          this.teacher = r.data;
+          this.exercises = r.data;;
+          this.time2 = moment(this.exercises.time).format('L');
         }).catch((e)=>{
           alert('error: '+e);
         });
       },
       load(){
-        this.getTeacher();
         this.getCourse();
         this.getExercise();
         this.check();
